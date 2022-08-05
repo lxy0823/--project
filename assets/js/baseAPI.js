fdd9b5358b733d248f4a3a12f4369e51da8d5f2a
1 <1103836770@qq.com>
2022-08-05 10:40:32 +0800

    文章类别功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,19 +1,23 @@
 // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
-$.ajaxPrefilter((option) => {
-    if (option.url.includes("/my/")) {
-        option.headers = {
-            Authorization: localStorage.getItem("token"),
-        };
-    }
-    // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-    option.url = `http://www.liulongbin.top:3007` + option.url;
 
-    option.complete = res => {
-        if (res.responseJSON.status === 1 && res.responseJSON.data.message === "身份认证失败！") {
-            location.href = "/login.html"
-            localStorage.removeItem("token")
+const baseUrl = 'http://big-event-api-t.itheima.net'
+
+$.ajaxPrefilter(option => {
+    // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
+    if (option.url.includes('/my/')) {
+        option.headers = {
+            Authorization: localStorage.getItem('token'),
         }
     }
-});
+    option.url = baseUrl + option.url
+
+    option.complete = res => {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            location.href = '/login.html'
+            // 清空本地存储里面的 token
+            localStorage.removeItem('token')
+        }
+    }
+})
